93e4ced585a358c3b6432dc2a86b5c6030e8236a
dancev2k <kobtsev.daniil03@gmail.com>
2026-04-16 23:02:21 +0300

    added reviews-page

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,18 @@ const swiper = new Swiper('.accessories__slider', {
     prevEl: '.accessories__button-prev',
   },
 });
+
+const swiperReviews = new Swiper('.reviews__slider', {
+  loop: true,
+  slidesPerView: 9,
+  spaceBetween: 15,
+
+  pagination: {
+    el: ".reviews__pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: '.reviews__button-next',
+    prevEl: '.reviews__button-prev',
+  },
+});
