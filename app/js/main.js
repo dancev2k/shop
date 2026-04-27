1e6eb9bacdca8f4c7a4f58137e1fb463c2939a41
dancev2k <kobtsev.daniil03@gmail.com>
2026-04-27 12:05:21 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,111 @@
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+const openModalBtn = document.querySelector('.open-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+
+function openModal() {
+  modal.setAttribute('aria-hidden', 'false')
+  document.body.classList.add('scroll-lock')
+}
+
+function closeModal() {
+  modal.setAttribute('aria-hidden', 'true')
+  document.body.classList.remove('scroll-lock')
+}
+
+if (modalOverlay) {
+  modalOverlay.addEventListener('click', closeModal)
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modal.getAttribute('aria-hidden') === 'false') {
+    closeModal()
+  }
+})
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+const popup = document.querySelector('.popup');
+const popupOverlay = document.querySelector('.popup__overlay');
+const openPopupBtn = document.querySelector('.catalog__popup');
+const closePopupBtn = document.querySelector('.close-popup');
+
+function openPopup() {
+  popup.setAttribute('aria-hidden', 'false')
+  document.body.classList.add('scroll-lock')
+}
+
+function closePopup() {
+  popup.setAttribute('aria-hidden', 'true')
+  document.body.classList.remove('scroll-lock')
+}
+
+if (popupOverlay) {
+  popupOverlay.addEventListener('click', closePopup)
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && popup.getAttribute('aria-hidden') === 'false') {
+    closePopup()
+  }
+})
+
+openPopupBtn?.addEventListener('click', openPopup);
+closePopupBtn?.addEventListener('click', closePopup);
+
+const breakpoint = window.matchMedia ('(min-width: 650px)');
+let sliderMobile = null;
+
+function initSwiper() {
+  sliderMobile = new Swiper('.slider-mobile', {
+    loop: true,
+    slidesPerView: 1,
+    centeredSlider: true,
+
+    navigation: {
+      nextEl: '.product__button-next',
+      prevEl: '.product__button-prev',
+    }
+  });
+}
+
+function destroySwiper() {
+  if (sliderMobile) {
+    sliderMobile.destroy(true, true);
+    sliderMobile = null;
+  }
+}
+
+function handleBreakpointChange(e) {
+  if (e.matches) {
+    destroySwiper()
+  } else {
+    if (!sliderMobile) initSwiper();
+  }
+}
+
+handleBreakpointChange(breakpoint);
+breakpoint.addEventListener('change', handleBreakpointChange);
+
+const menu = document.querySelector('.menu');
+const headerBtn = document.querySelector('.header__btn')
+
+headerBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+})
+
 const modeContainer = document.querySelector('.view-mode__container');
 const modeBtnGrid = document.querySelector('.view-mode__btn-grid');
 const modeBtnLine = document.querySelector('.view-mode__btn-line');
 
-modeBtnGrid.addEventListener('click', () => {
+modeBtnGrid?.addEventListener('click', () => {
   modeContainer.classList.add('view-mode__container--grid')
   modeContainer.classList.remove('view-mode__container--line')
   modeBtnLine.classList.add('view-mode__btn-line-disabled')
   modeBtnGrid.classList.remove('view-mode__btn-grid-disabled')
 });
-modeBtnLine.addEventListener('click', () => {
+modeBtnLine?.addEventListener('click', () => {
   modeContainer.classList.add('view-mode__container--line')
   modeContainer.classList.remove('view-mode__container--grid')
   modeBtnGrid.classList.add('view-mode__btn-grid-disabled')
@@ -25,6 +122,19 @@ const swiper = new Swiper('.accessories__slider', {
     nextEl: '.accessories__button-next',
     prevEl: '.accessories__button-prev',
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
 });
 
 const swiperReviews = new Swiper('.reviews__slider', {
@@ -39,6 +149,16 @@ const swiperReviews = new Swiper('.reviews__slider', {
   navigation: {
     nextEl: '.reviews__button-next',
     prevEl: '.reviews__button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 6,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 7,
+      spaceBetween: 15,
+    },
   },
 });
 
